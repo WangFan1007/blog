05f897b92dbf1e2968809024b8bc0cf9de066e33
wangfan <1036815077@qq.com>
2019-03-25 21:48:51 +0800

    表操作finished

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Input;
+use DB;
 
 class TestController extends Controller
 {
@@ -23,4 +24,60 @@ class TestController extends Controller
         dump(Input::has('name'));
 
     }
+
+    public function add()
+    {
+        $db = DB::table('member');
+        $rst = $db->insert([
+            [
+                'm_name' => '张三',
+                'age' => '23',
+                'email' => 'zhangsan@itcast.cn'
+            ],
+            [
+                'm_name' => '李四',
+                'age' => '24',
+                'email' => 'lisi@itcast.cn'
+            ],
+            [
+                'm_name' => '王五',
+                'age' => '25',
+                'email' => 'wangwu@itcast.cn'
+            ],
+        ]);
+        dump($rst);
+    }
+    public function update()
+    {
+        $db = DB::table('member');
+        $rst = $db->where('id','=','1')->update(['m_name' => '张三丰']);
+        dump($rst);
+    }
+
+    public function select()
+    {
+        $db = DB::table('member');
+        $data = $db->get();
+        // dump($data);
+        foreach ($data as $key => $value) {
+            // dump($value);
+            echo "key:{$key} id:{$value->id} m_name: {$value->m_name} age:{$value->age} email:{$value->email}<br>";
+        }
+    }
+    
+    public function del()
+    {
+        $db = DB::table('member');
+        $res = $db->where('id','<','3')->delete();
+        dump($res);
+    }
+    /**
+     * 
+     * 任意的sql语句 insert update delete (影响记录的)
+     * DB::statement("insert into member values('wang',20,'1036815077@qq.com')")
+     * 
+     * 任意select语句(不影响记录的)
+     * DB::select('select * from member')
+     * 
+     *  */
 }
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,10 @@ Route::get('/', function () {
 
 Route::get('/home/test/test1', 'TestController@test1');
 Route::get('/home/test/test2', 'TestController@test2');
+
+Route::group(['prefix' => 'home/test'], function () {
+    Route::get('add', 'TestController@add');
+    Route::get('del', 'TestController@del');
+    Route::get('update', 'TestController@update');
+    Route::get('select', 'TestController@select');
+});
